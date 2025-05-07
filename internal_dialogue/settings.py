aca29186191fa4be2f7df84fd4ac2aeedc9f2283
Dipto755 <akteruzzamandipto257@gmail.com>
2025-05-07 14:43:44 +0600

    cors allow all origins

--- internal_dialogue/settings.py
+++ internal_dialogue/settings.py
@@ -165,11 +165,13 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://192.168.10.12:3000",
-    "http://192.168.10.38:3000",
-    "https://flash-lubit-frontend.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://192.168.10.12:3000",
+#     "http://192.168.10.38:3000",
+#     "https://flash-lubit-frontend.vercel.app",
+# ]
 
 # SPECTACULAR_SETTINGS = {
 #     "SWAGGER_UI_DIST": "SIDECAR",  # shorthand to use the sidecar instead
